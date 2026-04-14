9a716f95387b7375d579c093e7d3b97994e4ca18
Antonio Sanchez <Antonio.Sanchez@analog.com>
2026-04-14 12:30:03 +0200

    Update service worker cache version to force refresh

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'numble-v1';
+const cacheName = 'numble-v2';
 const staticAssets = [
   './',
   './index.html',
@@ -10,6 +10,19 @@ self.addEventListener('install', async e => {
   const cache = await caches.open(cacheName);
   await cache.addAll(staticAssets);
   return self.skipWaiting();
+});
+
+// Activar y limpiar cachés antiguas
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== cacheName)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 // Responder desde la caché si no hay internet
